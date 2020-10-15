addbcd2b1fc1e5da9d82a0ce5d198b8a86512446
Christian <christian.nakata.acbs@gmail.com>
2020-10-15 17:23:32 -0300

    Refactored chapter 2

--- Chapter02. Perceptron/pg025_simplePerceptron.py
+++
@@ -1,8 +0,0 @@
-def AND (x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7 # Attribute values of weight 1, 2 and theta
-    tmp = x1 * w1 + x2 * w2
-
-    if tmp <= theta:
-        return 0
-    else:
-        return 1
